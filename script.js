edde24bbdf6a123944b6cc25b1ec2bd06141b20e
Rafiq <jalil716@gmail.com>
2025-04-07 20:29:55 -0700

    The push method lets you add to the end of the index while pop removes the last index. I added and removed items from the shopping cart and console.log to return the new list

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+//let groceryList = ["Apple", "Orange", "Plum", "Eggs", "Chicken"];
+//console.log(groceryList.length);
+
+
+//groceryList[2] = null;  // "Plum" is at index 2
+//groceryList[4] = null;  // "Chicken" is at index 4
+
+//groceryList.length = groceryList.length + 2;
+//groceryList.splice(2, 2, "Banana", "Grapes");
+
+//console.log(groceryList.length);
+
+let shoppingCart = [];
+
+shoppingCart.push("Steak", "Chicken", "Eggs");
+console.log('shoppingCart')
+
+shoppingCart.pop(-1);
+console.log(shoppingCart)
+
+let removeItem = shoppingCart.pop();
+console.log(shoppingCart.length)
+
+
+
